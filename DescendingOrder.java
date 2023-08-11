995c650a8160b2a2d21beb4ee32030b552dda56c
deliriumgrim <deliriumgrim@yandex.ru>
2023-08-11 15:18:36 +0300

    add exception

--- DescendingOrder.java
+++ DescendingOrder.java
@@ -2,6 +2,9 @@ import java.util.Arrays;
 
 public class DescendingOrder {
     public static int sortDesc(final int num) {
+        if (num < 0) {
+            throw new RuntimeException("Number must be positive");
+        }
         char[] charArray = String.valueOf(num).toCharArray();
         Arrays.sort(charArray);
         StringBuilder stringBuilder = new StringBuilder(new String(charArray));
